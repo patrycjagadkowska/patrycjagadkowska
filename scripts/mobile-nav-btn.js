6f808b0b38775171e1580301665afb1494e590b6
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-08-29 15:19:05 +0100

    add click listener to hamburger button

---
+++ scripts/mobile-nav-btn.js
@@ -0,0 +1,15 @@
+"use strict";
+const mobileNavBtn = document.getElementById("mobile-nav-btn");
+const mobileNav = document.getElementById("mobile-nav");
+function toggleNav() {
+    const isOpen = mobileNav.classList.contains("is-open");
+    if (isOpen) {
+        mobileNav.classList.remove("is-open");
+        mobileNavBtn.classList.remove("is-open");
+    }
+    else {
+        mobileNav.classList.add("is-open");
+        mobileNavBtn.classList.add("is-open");
+    }
+}
+mobileNavBtn.addEventListener("click", toggleNav);
